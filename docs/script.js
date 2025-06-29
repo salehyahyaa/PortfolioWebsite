599a3930ff2913b988d3c5ae3bb9c58b5e1d3551
Saleh Yahya <salehyahya10.20@gmail.com>
2025-06-29 11:20:49 -0500

    Fixed wave on mobile

--- docs/script.js
+++ docs/script.js
@@ -10,12 +10,19 @@ function toggleMenu() {
 window.onload = () => {
   const waveEmoji = document.querySelector(".wave");
 
+  // Desktop hover
   waveEmoji.addEventListener("mouseenter", () => {
-    console.log("hover detected");
     waveEmoji.style.animation = "waveAnimation 1.5s ease-in-out infinite";
   });
-
   waveEmoji.addEventListener("mouseleave", () => {
+    waveEmoji.style.animation = "none";
+  });
+
+  // Mobile touch
+  waveEmoji.addEventListener("touchstart", () => {
+    waveEmoji.style.animation = "waveAnimation 1.5s ease-in-out infinite";
+  });
+  waveEmoji.addEventListener("touchend", () => {
     waveEmoji.style.animation = "none";
   });
 };
