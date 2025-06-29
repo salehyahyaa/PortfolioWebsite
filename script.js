8b15d90d4aeff8a11f38d825662c8d22f2e93b1f
Saleh Yahya <salehyahya10.20@gmail.com>
2025-06-29 10:35:21 -0500

    Fixed Port

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links"); /*targetting this element*/ 
-  const icon = document.querySelector(".hamburger-icon"); /*and targetting this element*/ 
-  menu.classList.toggle("open"); /*so we can add or remove this class (open)*/ 
-  icon.classList.toggle("open"); /*so we can add or remove this class (open)*/ 
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+/* -------------------------------------------------- WAVE EMOJI -------------------------------------------------- */
+
+window.onload = () => {
+  const waveEmoji = document.querySelector(".wave");
+
+  waveEmoji.addEventListener("mouseenter", () => {
+    console.log("hover detected");
+    waveEmoji.style.animation = "waveAnimation 1.5s ease-in-out infinite";
+  });
+
+  waveEmoji.addEventListener("mouseleave", () => {
+    waveEmoji.style.animation = "none";
+  });
+};
